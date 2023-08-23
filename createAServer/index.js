36ba37447a0ff84624959d06859f4e98a77e6d4f
ealikan <ethem_alikan@hotmail.com>
2023-08-24 00:35:05 +0300

    reqres döngüsü

--- createAServer/index.js
+++ createAServer/index.js
@@ -1,3 +1,30 @@
 const http = require("http")
 
-const server = http.createServer()
+const server = http.createServer((req,res)=>{
+
+    const url = req.url
+
+    if(url ==="/"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write("<h1>index sayfasi</h1>")
+    }else if(url ==="/about"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write("<h1>about sayfasi<h1>")
+    }else if (url ==="/contact"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write("<h1>contact sayfasi<h1>")
+    }else{
+        res.writeHead(404,{"Content-Type":"text/html"})
+        res.write("<h1>404 NOT FOUND</h1>")
+    }
+    
+
+    
+    res.end()
+})
+
+const port = 8000
+
+server.listen(port,()=>{
+    console.log(`sunucu port ${port} de başlatıldı.`)
+})
